6125442d88f848d41b075f2d0ee6fc8ed5149e22
Alvin Tan <alvintan626@berkeley.edu>
2018-02-20 17:37:33 -0800

    Update water-array.js

--- water-array.js
+++ water-array.js
@@ -1,5 +1,7 @@
 var x = 230;
 var y = 60;
+var yy = 60;
+var yyy = 60;
 
 function setup() {
   createCanvas(400, 400);
@@ -19,18 +21,25 @@ function draw() {
   
   // draw drip
   var e1 = ellipse(230, y, 10);
-	var e2 = ellipse(230, 2*y, 10);
-	var e3 = ellipse(230, 1.5*y, 10);
+	var e2 = ellipse(230, yy, 10);
+	var e3 = ellipse(230, yyy, 10);
 	
   
   // down 3 pixels each frame, but maybe should be accelerating?
-  y = y + random(0,10)
+  y = y + random(0,3);
+	yy = yy + random(4,7);
+  yyy = yyy + random(8,10);
+
   
   // if invisible for a full “height” amount…
   if (y > height*2) {
     // reset
-		var i = i + 1;
-		ellipse(230,y,10)
-    y = 60;
-  }
+     y = 60;
+  } else if (yy > height*2) {
+    // reset
+	  yy = 60;
+	} else if (yyy > height*2) {
+    // reset
+	  yyy = 60;
+	}
 }
